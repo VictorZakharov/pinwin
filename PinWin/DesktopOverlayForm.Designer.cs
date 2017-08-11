3795a79d90dd5e4bf9fe270a5c66102e9d928a66
Neolisk <neolisk@gmail.com>
2017-08-11 16:28:39 -0400

    Fix screen capture overlay not working correctly on some multi monitor configurations.

--- PinWin/DesktopOverlayForm.Designer.cs
+++ PinWin/DesktopOverlayForm.Designer.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using System.ComponentModel;
-using System.Drawing;
+﻿using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace PinWin
 {
-  sealed partial class DesktopOverlayForm
-  {
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private IContainer components = null;
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    sealed partial class DesktopOverlayForm
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_DebugInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_DebugInfo
+            // 
+            this.lbl_DebugInfo.AutoSize = true;
+            this.lbl_DebugInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DebugInfo.ForeColor = System.Drawing.Color.Red;
+            this.lbl_DebugInfo.Location = new System.Drawing.Point(32, 32);
+            this.lbl_DebugInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DebugInfo.Name = "lbl_DebugInfo";
+            this.lbl_DebugInfo.Size = new System.Drawing.Size(203, 46);
+            this.lbl_DebugInfo.TabIndex = 0;
+            this.lbl_DebugInfo.Text = "DebugInfo";
+            // 
+            // DesktopOverlayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 448);
+            this.Controls.Add(this.lbl_DebugInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DesktopOverlayForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "DesktopOverlay";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DesktopOverlayForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DesktopOverlayForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DesktopOverlayForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_DebugInfo;
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-      this.lbl_DebugInfo = new Label();
-      this.SuspendLayout();
-      // 
-      // lbl_DebugInfo
-      // 
-      this.lbl_DebugInfo.AutoSize = true;
-      this.lbl_DebugInfo.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
-      this.lbl_DebugInfo.ForeColor = Color.Red;
-      this.lbl_DebugInfo.Location = new Point(24, 26);
-      this.lbl_DebugInfo.Name = "lbl_DebugInfo";
-      this.lbl_DebugInfo.Size = new Size(164, 37);
-      this.lbl_DebugInfo.TabIndex = 0;
-      this.lbl_DebugInfo.Text = "DebugInfo";
-      // 
-      // DesktopOverlayForm
-      // 
-      this.AutoScaleDimensions = new SizeF(6F, 13F);
-      this.AutoScaleMode = AutoScaleMode.Font;
-      this.ClientSize = new Size(556, 364);
-      this.Controls.Add(this.lbl_DebugInfo);
-      this.FormBorderStyle = FormBorderStyle.None;
-      this.Name = "DesktopOverlayForm";
-      this.ShowInTaskbar = false;
-      this.Text = "DesktopOverlay";
-      this.KeyDown += new KeyEventHandler(this.DesktopOverlayForm_KeyDown);
-      this.MouseDown += new MouseEventHandler(this.DesktopOverlayForm_MouseDown);
-      this.MouseMove += new MouseEventHandler(this.DesktopOverlayForm_MouseMove);
-      this.ResumeLayout(false);
-      this.PerformLayout();
-
-    }
-
-    #endregion
-
-    private Label lbl_DebugInfo;
-  }
 }
--- PinWin/DesktopOverlayForm.cs
+++ PinWin/DesktopOverlayForm.cs
@@ -1,81 +1,90 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+
 using PinWin.BusinessLayer;
 using PinWin.Controls;
 
 namespace PinWin
 {
-  public partial class DesktopOverlayForm : OverlayForm
-  {
-#region " Properties "
-    private Nullable<Point> _mouseClickPoint = null;
-
-    public Nullable<Point> MouseClickPoint => _mouseClickPoint;
-#endregion
-
-#region " Constructor "
-    public DesktopOverlayForm()
+    public partial class DesktopOverlayForm : OverlayForm
     {
-      InitializeComponent();
+        #region " Properties "
 
-      //set overlay form as full screen across all monitors
-      this.Bounds = ScreenCapture.GetDesktopBounds();
+        private Point? _mouseClickPoint = null;
 
-      //set desktop overlay image
-      this.BackgroundImage = ScreenCapture.CreateBitmapFromDesktop();
+        public Point? MouseClickPoint => _mouseClickPoint;
 
-      //apply custom cursor - make interface more user-friendly
-      this.Cursor = new Cursor(EmbeddedResource.TargetWindowIcon.Handle);
+        #endregion
+
+        #region " Constructor "
+
+        public DesktopOverlayForm()
+        {
+            InitializeComponent();
+
+            //set overlay form as full screen across all monitors
+            this.Bounds = ScreenCapture.GetDesktopBounds();
+
+            //set desktop overlay image
+            this.BackgroundImage = ScreenCapture.CreateBitmapFromDesktop();
+
+            //apply custom cursor - make interface more user-friendly
+            this.Cursor = new Cursor(EmbeddedResource.TargetWindowIcon.Handle);
+        }
+
+        #endregion
+
+        #region " Public methods "
+
+        /// <summary>
+        ///  Prompt user to select a Point on screen.
+        /// </summary>
+        /// <param name="parentForm">
+        ///  A new window will open for this parent form.
+        /// </param>
+        /// <returns>
+        ///  Point that was selected by user. Null if selection was cancelled.
+        /// </returns>
+        public static Point? SelectPoint(Form parentForm)
+        {
+            var desktopOverlayForm = new DesktopOverlayForm();
+            desktopOverlayForm.ShowDialog(parentForm);
+            return desktopOverlayForm.MouseClickPoint;
+        }
+
+        #endregion
+
+        #region " Event handlers "
+
+        /// <summary>
+        ///  User-friendly interface, escape key triggers form close.
+        /// </summary>
+        private void DesktopOverlayForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        ///  Mouse coordinates will be temporarily saved for debugging purposes.
+        /// </summary>
+        private void DesktopOverlayForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            lbl_DebugInfo.Text = $@"Mouse - X: {e.X}, Y: {e.Y}";
+        }
+
+        /// <summary>
+        ///  Default behavior - user clicks a point, coordinates stored, form is closed.
+        /// </summary>
+        private void DesktopOverlayForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            this._mouseClickPoint = new Point(e.X, e.Y);
+            this.Close();
+        }
+
+        #endregion
     }
-    #endregion
-
-#region " Public methods "
-    /// <summary>
-    ///  Prompt user to select a Point on screen.
-    /// </summary>
-    /// <param name="parentForm">
-    ///  A new window will open for this parent form.
-    /// </param>
-    /// <returns>
-    ///  Point that was selected by user. Null if selection was cancelled.
-    /// </returns>
-    public static Nullable<Point> SelectPoint(Form parentForm)
-    {
-      var desktopOverlayForm = new DesktopOverlayForm();
-      desktopOverlayForm.ShowDialog(parentForm);
-      return desktopOverlayForm.MouseClickPoint;
-    }
-#endregion
-
-#region " Event handlers "
-    /// <summary>
-    ///  User-friendly interface, escape key triggers form close.
-    /// </summary>
-    private void DesktopOverlayForm_KeyDown(object sender, KeyEventArgs e)
-    {
-      if (e.KeyCode == Keys.Escape)
-      {
-        this.Close();
-      }
-    }
-
-    /// <summary>
-    ///  Mouse coordinates will be temporarily saved for debugging purposes.
-    /// </summary>
-    private void DesktopOverlayForm_MouseMove(object sender, MouseEventArgs e)
-    {
-      lbl_DebugInfo.Text = $"Mouse - X: {e.X}, Y: {e.Y}";
-    }
-
-    /// <summary>
-    ///  Default behavior - user clicks a point, coordinates stored, form is closed.
-    /// </summary>
-    private void DesktopOverlayForm_MouseDown(object sender, MouseEventArgs e)
-    {
-      this._mouseClickPoint = new Point(e.X, e.Y);
-      this.Close();
-    }
-#endregion
-  }
 }
