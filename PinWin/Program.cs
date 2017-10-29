2b7690f117c83b7f26cdd3538c872ac97de1281e
Neolisk <neolisk@gmail.com>
2017-10-29 14:44:49 -0400

    Fix most recent issue in #9 (DPI awareness for screen capture tool).

--- PinWin/Program.cs
+++ PinWin/Program.cs
@@ -3,17 +3,30 @@ using System.Windows.Forms;
 
 namespace PinWin
 {
-  static class Program
-  {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    static class Program
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainForm());
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        /// <summary>
+        ///  Sets process as DPI aware, prevents odd behavior related to auto-scaling.
+        /// </summary>
+        /// <remarks>
+        ///  https://stackoverflow.com/questions/13228185/how-to-configure-an-app-to-run-correctly-on-a-machine-with-a-high-dpi-setting-e/13228495#13228495
+        /// </remarks>
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
-  }
 }
