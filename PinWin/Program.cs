4d8c3494c830bec51b9fc660d133ac4f5fdeaaf8
Neolisk <neolisk@gmail.com>
2017-11-26 20:54:22 -0500

    Fix issue #10 - PinWin is now a singleton.

--- PinWin/Program.cs
+++ PinWin/Program.cs
@@ -13,11 +13,25 @@ namespace PinWin
         {
             if (Environment.OSVersion.Version.Major >= 6)
             {
+                // prevent overlay from capturing only a portion of the desktop
                 SetProcessDPIAware();
             }
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            using (var app = new SingleGlobalInstance())
+            {
+                const int singleInstanceWaitMs = 1000;
+                if (!app.IsFirstInstance(singleInstanceWaitMs))
+                {
+                    // force single instance
+                    MessageBox.Show(@"An instance of PinWin is already running.");
+                    return;
+                }
+
+                // else we are good to run the app
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
 
         /// <summary>
---
+++ PinWin/SingleGlobalInstance.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+
+namespace PinWin
+{
+    /// <summary>
+    ///  Forces to the application to run in single instance mode.
+    /// </summary>
+    /// <remarks>
+    ///  Used code from here https://stackoverflow.com/a/7810107/897326 as a base.
+    /// </remarks>
+    class SingleGlobalInstance : IDisposable
+    {
+        private bool _hasHandle = false;
+        private Mutex _mutex;
+
+        /// <summary>
+        ///  Constructor.
+        /// </summary>
+        public SingleGlobalInstance()
+        {
+            InitMutex();
+        }
+
+        /// <summary>
+        ///  Check if first instance.
+        /// </summary>
+        /// <param name="timeout">Timeout in milliseconds.</param>
+        public bool IsFirstInstance(int timeout)
+        {
+            InitMutex();
+            try
+            {
+                var waitMs = timeout >= 0 ? timeout : Timeout.Infinite;
+                this._hasHandle = _mutex.WaitOne(waitMs, false);
+            }
+            catch (AbandonedMutexException)
+            {
+                // ignore error, still got the handle
+                this._hasHandle = true;
+            }
+            return this._hasHandle;
+        }
+
+        /// <summary>
+        ///  Implements IDisposable.
+        /// </summary>
+        public void Dispose()
+        {
+            if (_mutex != null)
+            {
+                if (_hasHandle)
+                    _mutex.ReleaseMutex();
+                _mutex.Close();
+            }
+        }
+
+        /// <summary>
+        ///  Initialize the mutex.
+        /// </summary>
+        private void InitMutex()
+        {
+            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().
+                GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value;
+
+            string mutexId = $"Global\\{{{appGuid}}}";
+            this._mutex = new Mutex(false, mutexId);
+
+            var allowEveryoneRule = new MutexAccessRule(
+                new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                MutexRights.FullControl, AccessControlType.Allow);
+
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+            this._mutex.SetAccessControl(securitySettings);
+        }
+    }
+}
